eea50c012b60674419bd11e0bef2e41e94fa3fb8
josephabirached <josephabirached0@gmail.com>
2020-12-15 14:35:27 +0100

    Test read() de la classe MarkovData

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -2,13 +2,37 @@ package markov;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class MarkovDataTest {
 	
-	//@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test
+	/*
+	 *  GIVEN_WHEN_THEN 
+	 */
+	public void read_text_List() {
+		
+		List<String> expected = new ArrayList<String>();
+		expected.add("Hello");
+		expected.add("this");
+		expected.add("is");
+		expected.add("a");
+		expected.add("test!");
+		
+		String text = "Hello this is a test!";
+		
+		MarkovData d = new MarkovData();
+		d.read(text);
+		
+		for(int i =0; i< expected.size(); i++) {
+			String actual = d.getKeyWord(i);
+			
+			assertEquals(expected.get(i), actual);
+		}
+		
 	}
 
 }
